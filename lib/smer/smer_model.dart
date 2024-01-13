45e4b032c48df7b2bbcca77454af1691235e8817
luka.gluscevic <luka.gluscevic@icbtech.rs>
2024-01-13 18:47:02 +0100

    added token

--- lib/smer/smer_model.dart
+++ lib/smer/smer_model.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
@@ -19,13 +18,20 @@ class Smer {
 // smer_api.dart
 
 Future<List<Smer>> getSmerovi() async {
-  final response = await http.get(Uri.parse('https://pmappbk2.ddns.net/Smer/get_smerovi'));
+  const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IkdsdXNjZXZpYyIsIm5hbWVpZCI6ImdsdXNjZXZpY2x1a2FAZ21haWwuY29tIiwiSWQiOiI5OSIsIlRpbWVTdGFtcCI6IjEvMTMvMjAyNCA2OjM3OjQxIFBNIiwibmJmIjoxNzA1MTY3NDYxLCJleHAiOjE3MDUxOTYyNjEsImlhdCI6MTcwNTE2NzQ2MX0.omIBkOzkA_gJuuDuuYg9guTSfltlK3H2AJZdxzct76w';
+  final response = await http.get(
+    Uri.parse('https://pmappbk2.ddns.net/Smer/get_smerovi'),
+    headers: {'Authorization': 'Bearer $token'},
+  );
 
   if (response.statusCode == 200) {
-    List<dynamic> jsonData = json.decode(response.body);
-    List<Smer> smerovi = jsonData.map((json) => Smer.fromJson(json)).toList();
+    // Ako je status OK (200), parsiraj odgovor
+    List<dynamic> data = json.decode(response.body);
+    List<Smer> smerovi = data.map((item) => Smer.fromJson(item)).toList();
     return smerovi;
   } else {
+    // Ako nije OK, baci izuzetak
     throw Exception('Failed to load smerovi');
   }
 }
